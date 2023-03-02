80745158f61f8b952e6a72b733596f6e40e5b7cb
Per E Kummervold <per@capia.no>
2023-03-02 14:49:45 +0100

    update

---
+++ experiment_scream_prime/run_scream_prime_nccsnrk_constantlr_tiny.sh
@@ -0,0 +1,24 @@
+python run_flax_speech_recognition_seq2seq_streaming.py \
+            --model_name_or_path openai/whisper-tiny \
+            --dataset_name NbAiLab/NCC_S3_nrk \
+	        --language Norwegian \
+            --text_column_name text \
+            --train_split_name train\
+            --eval_split_name test\
+            --output_dir ../../scream_prime_nccnrk_constantlr_tiny\
+            --overwrite_output_dir\
+            --warmup_steps 20 \
+            --do_train \
+            --do_eval \
+            --num_train_steps 1000 \
+            --eval_steps 5 \
+            --learning_rate 1.5e-3 \
+            --per_device_train_batch_size 64 \
+            --per_device_eval_batch_size 12 \
+            --predict_with_generate \
+            --number_write_predictions 100 \
+            --streaming True \
+            --hub_private_repo True \
+            --push_to_hub_organization NbAiLab \
+            --use_auth_token True \
+            --push_to_hub
