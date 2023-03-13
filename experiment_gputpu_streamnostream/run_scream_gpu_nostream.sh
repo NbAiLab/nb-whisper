340fe127428294d0ff5b4a5868fb99920eb7da1f
Per Egil Kummervold <per@capia.no>
2023-03-13 22:22:05 +0000

    test

--- experiment_gputpu_streamnostream/run_scream_gpu_nostream.sh
+++ experiment_gputpu_streamnostream/run_scream_gpu_nostream.sh
@@ -14,7 +14,7 @@ python ../run_flax_speech_recognition_seq2seq.py \
             --warmup_steps 100 \
             --do_train \
             --do_eval \
-            --num_epochs 1
+            --num_epochs 1 \
             --eval_steps 100 \
             --learning_rate 0.75e-3 \
             --per_device_train_batch_size 256 \
