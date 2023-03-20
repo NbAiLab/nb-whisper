0af5ed6dd086e464ef60464c364bda4afe79c2a0
Per Egil Kummervold <per@capia.no>
2023-03-20 21:51:46 +0000

    bbs

--- experiment_scream_tertius/run_scream_exp13_constand_bbs_1e5.sh
+++ experiment_scream_tertius/run_scream_exp13_constand_bbs_1e5.sh
@@ -9,23 +9,23 @@ python ../run_flax_speech_recognition_seq2seq_streaming.py \
             --text_column_name text \
             --train_split_name train\
             --eval_split_name test\
-            --output_dir ../../scream_tertius_exp5_constant_075e3\
+            --output_dir ../../scream_tertius_exp13_constant_bbs_1e5\
             --overwrite_output_dir\
             --warmup_steps 1000 \
             --do_train \
             --do_eval \
-            --num_train_steps 50000 \
+            --num_train_steps 100000 \
 	    --lr_scheduler_type constant_with_warmup \
             --eval_steps 500 \
-            --learning_rate 0.75e-3 \
+            --learning_rate 1e-5 \
 	    --preprocessing_num_workers 50 \
-            --per_device_train_batch_size 64 \
+            --per_device_train_batch_size 199 \
             --per_device_eval_batch_size 64 \
             --predict_with_generate \
             --number_write_predictions 100 \
             --streaming True \
             --hub_private_repo True \
-            --hub_model_id NbAiLab/scream_tertius_exp5_constant_075e3 \
+            --hub_model_id NbAiLab/scream_tertius_exp13_constant_bbs_1e5 \
             --use_auth_token True \
             --dtype bfloat16 \
             --push_to_hub
