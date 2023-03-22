8404b3d6ff20c7cb6856198ae91c297dd02fad17
Per E Kummervold <per@capia.no>
2023-03-22 07:43:56 +0100

    test

--- run_streaming.sh
+++ run_streaming.sh
@@ -1,22 +1,30 @@
+export TOKENIZERS_PARALELLISM=false
+export CMALLOC_VERBOSE=0
+export TCMALLOC_VERBOSE=0
+export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 python run_flax_speech_recognition_seq2seq_streaming_dev.py \
             --model_name_or_path openai/whisper-tiny \
-            --dataset_name mozilla-foundation/common_voice_11_0 \
-            --dataset_config nn-NO \
-	    --language Norwegian \
-            --text_column_name sentence \
-            --train_split_name test\
+            --dataset_name NbAiLab/NCC_speech_nrk_v4 \
+	        --language Norwegian \
+            --text_column_name text \
+            --train_split_name validation\
             --eval_split_name test\
-            --output_dir ../../whisper-tiny-ft-dummy\
+            --output_dir ../../whisper_dummy\
             --overwrite_output_dir\
-            --max_eval_samples=16\
+            --max_eval_samples=20\
             --warmup_steps=8 \
             --do_train \
             --do_eval \
             --num_train_steps 30 \
             --eval_steps 5 \
-            --learning_rate=2e-4 \
+            --learning_rate=1e-5 \
             --per_device_train_batch_size=2 \
             --per_device_eval_batch_size=2 \
+            --streaming True \
+            --hub_private_repo True \
+            --hub_model_id NbAiLab/whisper_dummy \
+            --use_auth_token True \
+            --dtype bfloat16 \
             --predict_with_generate \
             --log_eval_predictions_fn log_predictions.write_predictions \
             --log_max_eval_predictions 100 \
