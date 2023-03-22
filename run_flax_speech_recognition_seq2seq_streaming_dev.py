e3d077867236c6e682522e3ca7510d8fc48d9f6b
Per Egil Kummervold <per@capia.no>
2023-03-22 18:11:45 +0000

    test

--- run_flax_speech_recognition_seq2seq_streaming_dev.py
+++ run_flax_speech_recognition_seq2seq_streaming_dev.py
@@ -535,7 +535,7 @@ def main():
         logger.info(
             f"Checkpoint detected, resuming training at {training_args.output_dir}."
         )
-        model_name_or_path = os.path.join(training_args.output_dir, "flax_model.msgpack")
+        model_name_or_path = os.path.join(training_args.output_dir)
     else:
         logger.info(
             f"No valid checkpoint found in {training_args.output_dir}. Starting from {model_args.model_name_or_path}."
