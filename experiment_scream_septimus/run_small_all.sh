d4dd1ada9e9b9de057bede8fd04eba273f4c3ad6
Per Egil Kummervold <per@capia.no>
2023-04-09 18:40:36 +0000

    test

--- experiment_scream_septimus/run_small_all.sh
+++ experiment_scream_septimus/run_small_all.sh
@@ -2,29 +2,29 @@ export TOKENIZERS_PARALELLISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_flax_speech_recognition_seq2seq_streaming.py \
-        --model_name_or_path openai/whisper-medium \
-        --run_name "ScreamMedium - exp_sept_tpu64_medium_2e6" \
-        --run_description "A Large Scream model with 4*4*8=160 seq length. Trained with linear decay only on the all_v5 corpus. This version is trained with a learning rate of 2e6." \
+python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
+        --model_name_or_path openai/whisper-small \
+        --run_name "ScreamSmall - exp_sept_tpu32_all" \
+        --run_description "A Tiny Whisper Scream model with 32*4*4=512 seq length. Trained with linear decay on the all_v5-corpus." \
         --wandb_entity "nbailab" \
-        --wandb_project "Scream - septimus" \
+        --wandb_project "ScreamSmall - septimus" \
         --dataset_name NbAiLab/NCC_speech_all_v5 \
         --language Norwegian \
         --text_column_name text \
         --train_split_name train \
         --eval_split_name validation \
-        --output_dir ../../scream_medium_sept_2e6\
+        --output_dir ../../scream_small_sept_all\
         --overwrite_output_dir\
-        --warmup_steps 5000 \
+        --warmup_steps 10000 \
         --do_train \
         --do_eval \
-        --num_train_steps 100000 \
+        --num_train_steps 250000 \
         --lr_scheduler_type linear \
-        --eval_steps 2500 \
-        --learning_rate 2e-6 \
-        --preprocessing_num_workers 16 \
-        --per_device_train_batch_size 4 \
-        --per_device_eval_batch_size 4 \
+        --eval_steps 5000 \
+        --learning_rate 2e-5 \
+        --preprocessing_num_workers 64 \
+        --per_device_train_batch_size 16 \
+        --per_device_eval_batch_size 16 \
         --predict_with_generate \
         --log_max_eval_predictions 100 \
         --log_eval_predictions_fn "log_predictions.write_predictions" \
@@ -32,6 +32,6 @@ python ../run_flax_speech_recognition_seq2seq_streaming.py \
         --use_auth_token True \
         --dtype bfloat16 \
         --hub_private_repo True \
-        --hub_model_id NbAiLab/scream_medium_sept_2e6 \
+        --hub_model_id NbAiLab/scream_small_sept_all \
         --resume_from_checkpoint True \
         --push_to_hub
