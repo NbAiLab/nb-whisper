eb977fceac3ad4074023af07d127a590c480b898
Per Egil Kummervold <per@capia.no>
2023-04-10 08:22:31 +0000

    attention mask fix

--- run_flax_speech_recognition_seq2seq_streaming.py
+++ run_flax_speech_recognition_seq2seq_streaming.py
@@ -418,6 +418,7 @@ class FlaxDataCollatorSpeechSeq2SeqWithPadding:
 
         batch["labels"] = labels
         batch["decoder_input_ids"] = decoder_input_ids
+        batch["attention_mask"] = labels_batch.attention_mask  # Add attention_mask to the batch
 
         return batch
 
