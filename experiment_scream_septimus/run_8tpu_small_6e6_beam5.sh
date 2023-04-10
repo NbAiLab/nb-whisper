d3e1b9c94bae631411e4e597a3b84773dfdd1cd3
Per Egil Kummervold <per@capia.no>
2023-04-10 09:45:05 +0000

    new large exp

--- experiment_scream_septimus/run_8tpu_small_6e6_beam5.sh
+++ experiment_scream_septimus/run_8tpu_small_6e6_beam5.sh
@@ -23,7 +23,7 @@ python ../run_flax_speech_recognition_seq2seq_streaming.py \
         --eval_steps 2500 \
         --learning_rate 6e-6 \
         --preprocessing_num_workers 32 \
-        --per_device_train_batch_size 40 \
+        --per_device_train_batch_size 32 \
         --per_device_eval_batch_size 8 \
         --predict_with_generate \
         --log_max_eval_predictions 100 \
