3a4804922b1a6f40bcd8ea8539c7fa09099eb92b
Per Egil Kummervold <per@capia.no>
2023-04-12 15:56:26 +0000

    debug

--- experiment_scream_octavus/run_small_debug.sh
+++ experiment_scream_octavus/run_small_debug.sh
@@ -13,18 +13,18 @@ python ../run_flax_speech_recognition_seq2seq_streaming_debug.py \
     --text_column_name text \
     --train_split_name train\
     --eval_split_name validation\
-    --output_dir ../../scream_small_octavus_debug\
+    --output_dir ../../scream_small_octavus_debug_beam\
     --overwrite_output_dir\
-    --warmup_steps 500 \
+    --warmup_steps 5000 \
     --do_train \
     --do_eval \
-    --num_train_steps 10000 \
+    --num_train_steps 50000 \
     --lr_scheduler_type linear \
-    --eval_steps 20000 \
-    --learning_rate 3e-5 \
+    --eval_steps 2500 \
+    --learning_rate 2e-5 \
     --preprocessing_num_workers 32 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --predict_with_generate \
     --log_max_eval_predictions 100 \
     --log_eval_predictions_fn "log_predictions.write_predictions" \
@@ -32,7 +32,8 @@ python ../run_flax_speech_recognition_seq2seq_streaming_debug.py \
     --use_auth_token True \
     --dtype bfloat16 \
     --hub_private_repo True \
-    --hub_model_id NbAiLab/scream_small_octavus_debug \
+    --hub_model_id NbAiLab/scream_small_octavus_debug_beam \
     --resume_from_checkpoint True \
     --ignore_data_skip True \
+    --num_beams 5 \
     --push_to_hub
