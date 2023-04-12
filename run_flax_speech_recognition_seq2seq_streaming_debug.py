fa0268851ed3fc6d15ff80861ea8545d725084c9
Per Egil Kummervold <per@capia.no>
2023-04-12 09:53:32 +0000

    debug

--- run_flax_speech_recognition_seq2seq_streaming_debug.py
+++ run_flax_speech_recognition_seq2seq_streaming_debug.py
@@ -1244,10 +1244,10 @@ def main():
         
         batch = shard(batch.data)
         
-        state_structure = jax.tree_map(lambda x: None, state)
+        #state_structure = jax.tree_map(lambda x: None, state)
         #print("---------------\nBefore updating state:")
         #print(f"Number of sequences per device: {len(batch['labels'][0])}")
-        print(f"Lengths of sequences: {[len(seq[0]) for seq in batch['labels']]}")
+        print(f"Lengths of label sequences: {[len(seq[0]) for seq in batch['labels']]}")
         
         #print(f"Example sequence label: {batch['labels'][0][0][:10]} ...")
         #print_structure(state_structure)
