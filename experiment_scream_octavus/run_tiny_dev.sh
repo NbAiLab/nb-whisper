73c131154948ac6d2cc79a414e7c5165527f7eac
Per Egil Kummervold <per@capia.no>
2023-04-13 12:52:11 +0000

    updates to dev

--- experiment_scream_octavus/run_tiny_dev.sh
+++ experiment_scream_octavus/run_tiny_dev.sh
@@ -2,29 +2,29 @@ export TOKENIZERS_PARALELLISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
+python ../run_flax_speech_recognition_seq2seq_streaming_demini.py \
     --model_name_or_path openai/whisper-tiny \
     --run_name "ScreamTiny - debug" \
     --run_description "A Tiny Whisper Scream model - Debug delete." \
     --wandb_entity "nbailab" \
     --wandb_project "Scream - octavus" \
-    --dataset_name NbAiLab/NCC_speech_all_v5 \
+    --dataset_name NbAiLab/NCC_speech_v5_mini \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train\
     --eval_split_name validation\
-    --output_dir ../../scream_tiny_octavus_dev\
+    --output_dir ../../scream_tiny_octavus_dev_mini\
     --overwrite_output_dir\
     --warmup_steps 100 \
     --do_train \
     --do_eval \
-    --num_train_steps 1100 \
+    --num_train_steps 1000 \
     --lr_scheduler_type linear \
     --eval_steps 200 \
     --learning_rate 3e-5 \
     --preprocessing_num_workers 32 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --predict_with_generate \
     --log_max_eval_predictions 100 \
     --log_eval_predictions_fn "log_predictions.write_predictions" \
@@ -32,7 +32,7 @@ python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
     --use_auth_token True \
     --dtype bfloat16 \
     --hub_private_repo True \
-    --hub_model_id NbAiLab/scream_tiny_octavus_dev \
+    --hub_model_id NbAiLab/scream_tiny_octavus_dev_mini \
     --resume_from_checkpoint True \
     --ignore_data_skip True \
     --push_to_hub
