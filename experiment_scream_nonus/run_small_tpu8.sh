cdc474ff6a0aaa94d0681e679c7bb34596f9953e
Per Egil Kummervold <per@capia.no>
2023-04-15 15:27:42 +0000

    updates to dev

--- experiment_scream_nonus/run_small_tpu8.sh
+++ experiment_scream_nonus/run_small_tpu8.sh
@@ -4,7 +4,7 @@ export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
         --model_name_or_path openai/whisper-small \
-        --run_name "Scream - small_tpu8_8e6_beam5" \
+        --run_name "Scream - small_non_tpu8_8e6_beam5" \
         --run_description "A Small Scream model. Trained with linear decay only on the all_v5 corpus. This version is trained with a learning rate of 6e6." \
         --wandb_entity "nbailab" \
         --wandb_project "Scream - septimus" \
--- experiment_scream_nonus/run_small_tpu8_dropout.sh
+++ experiment_scream_nonus/run_small_tpu8_dropout.sh
@@ -4,7 +4,7 @@ export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
         --model_name_or_path openai/whisper-small \
-        --run_name "Scream - small_non_tpu8_8e6_beam5" \
+        --run_name "Scream - small_non_tpu8_8e6_beam5_dropout01" \
         --run_description "A Small Scream model. Trained with linear decay only on the all_v5 corpus. This version is trained with a learning rate of 6e6." \
         --wandb_entity "nbailab" \
         --wandb_project "Scream - septimus" \
@@ -13,7 +13,7 @@ python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
         --text_column_name text \
         --train_split_name train \
         --eval_split_name validation \
-        --output_dir ../../scream_non_small_tpu8_8e6_beam5\
+        --output_dir ../../scream_non_small_tpu8_8e6_beam5_dropout01\
         --overwrite_output_dir\
         --warmup_steps 5000 \
         --do_train \
@@ -32,7 +32,7 @@ python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
         --use_auth_token True \
         --dtype bfloat16 \
         --hub_private_repo True \
-        --hub_model_id NbAiLab/scream_non_small_tpu8_8e6_beam5 \
+        --hub_model_id NbAiLab/scream_non_small_tpu8_8e6_beam5_dropout01 \
         --resume_from_checkpoint True \
         --num_beams 5 \
         --ignore_data_skip \
