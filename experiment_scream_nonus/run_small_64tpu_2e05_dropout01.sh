b74ea239e78f37c1aff8915a87030fa11a2478f7
Per Egil Kummervold <per@capia.no>
2023-04-16 10:08:48 +0000

    updates to dev

---
+++ experiment_scream_nonus/run_small_64tpu_2e05_dropout01.sh
@@ -0,0 +1,40 @@
+export TOKENIZERS_PARALELLISM=false
+export CMALLOC_VERBOSE=0
+export TCMALLOC_VERBOSE=0
+export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
+python ../run_flax_speech_recognition_seq2seq_streaming_dev.py \
+        --model_name_or_path openai/whisper-small \
+        --run_name "Scream - non_small_64pod_2e05_beam5_dropout01" \
+        --run_description "A Small Scream model. Trained with linear decay only on the all_v5 corpus. This version is trained with a learning rate of 6e6." \
+        --wandb_entity "nbailab" \
+        --wandb_project "Scream - septimus" \
+        --dataset_name NbAiLab/NCC_speech_all_v5 \
+        --language Norwegian \
+        --text_column_name text \
+        --train_split_name train \
+        --eval_split_name validation \
+        --output_dir ../../scream_non_small_64pod_2e05_beam5_dropout01\
+        --overwrite_output_dir\
+        --warmup_steps 2000 \
+        --do_train \
+        --do_eval \
+        --num_train_steps 20000 \
+        --lr_scheduler_type linear \
+        --eval_steps 1000 \
+        --learning_rate 2e-5 \
+        --preprocessing_num_workers 32 \
+        --per_device_train_batch_size 24 \
+        --per_device_eval_batch_size 4 \
+        --predict_with_generate \
+        --log_max_eval_predictions 100 \
+        --log_eval_predictions_fn "log_predictions.write_predictions" \
+        --streaming True \
+        --use_auth_token True \
+        --dtype bfloat16 \
+        --hub_private_repo True \
+        --hub_model_id NbAiLab/small_non_64pod_2e05_beam5_dropout01 \
+        --resume_from_checkpoint True \
+        --num_beams 5 \
+        --ignore_data_skip \
+        --dropout 0.1 \
+        --push_to_hub
