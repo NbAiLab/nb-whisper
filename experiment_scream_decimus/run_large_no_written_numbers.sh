8bace7dd6ac86aa3f5958b930725283c5e1a2e83
Per Egil Kummervold <per@capia.no>
2023-04-27 12:25:13 +0000

    debug version

--- experiment_scream_decimus/run_large_no_written_numbers.sh
+++ experiment_scream_decimus/run_large_no_written_numbers.sh
@@ -2,7 +2,7 @@ export TOKENIZERS_PARALELLISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
+python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered2.py \
     --model_name_or_path openai/whisper-large-v2 \
     --run_name "Scream - non_large_decimus_written_numbers" \
     --run_description "Decimus - decimus experiment all numerical numbers under 100 is filtered out." \
@@ -13,7 +13,7 @@ python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --text_column_name text \
     --train_split_name train\
     --eval_split_name validation\
-    --output_dir ../../scream_large_decimus_no_written \
+    --output_dir ../../scream_large_decimus_no_written_numbers \
     --overwrite_output_dir\
     --warmup_steps 2000 \
     --do_train \
@@ -32,7 +32,7 @@ python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --use_auth_token True \
     --dtype bfloat16 \
     --hub_private_repo True \
-    --hub_model_id NbAiLab/scream_large_decimus_no_written \
+    --hub_model_id NbAiLab/scream_large_decimus_no_written_numbers \
     --resume_from_checkpoint True \
     --ignore_data_skip \
     --num_beams 5 \
--- experiment_scream_decimus/run_large_spesial_characters.sh
+++ experiment_scream_decimus/run_large_spesial_characters.sh
@@ -2,7 +2,7 @@ export TOKENIZERS_PARALELLISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered2.py \
+python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --model_name_or_path openai/whisper-large-v2 \
     --run_name "Scream - non_large_decimus_special_characters" \
     --run_description "Decimus - experiment with special characters. Linebreaks etc. To see how they will render." \
