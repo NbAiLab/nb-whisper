31697936c1d260445fd4e67e17a0feb49cfef2be
Per Egil Kummervold <per@capia.no>
2023-05-12 11:17:35 +0000

    loss experiment

--- experiment_scream_decimus/run_debug.sh
+++ experiment_scream_decimus/run_debug.sh
@@ -2,7 +2,7 @@ export TOKENIZERS_PARALELLISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
+python ../run_nb_flax_speech_recognition_seq2seq_streaming_test.py \
     --model_name_or_path NbAiLab/scream_non_large_1e06_beams5_constantlr_long \
     --run_name "Scream - non_large_1e06_verbosity3" \
     --run_description "A Large Whisper Scream model with 5 batch size. Trained with 1e-6 and linear decay on the all_v5-corpus." \
@@ -13,7 +13,7 @@ python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --text_column_name text \
     --train_split_name train\
     --eval_split_name validation\
-    --output_dir ../../scream_non_large_debug3\
+    --output_dir ../../scream_non_large_debug4\
     --overwrite_output_dir\
     --warmup_steps 2500 \
     --do_train \
@@ -23,7 +23,7 @@ python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --eval_steps 2500 \
     --learning_rate 1e-6 \
     --preprocessing_num_workers 32 \
-    --per_device_train_batch_size 5 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
     --predict_with_generate \
     --log_max_eval_predictions 100 \
@@ -32,7 +32,7 @@ python ../run_nb_flax_speech_recognition_seq2seq_streaming_filtered3.py \
     --use_auth_token True \
     --dtype bfloat16 \
     --hub_private_repo True \
-    --hub_model_id NbAiLab/scream_non_large_debug33 \
+    --hub_model_id NbAiLab/scream_non_large_debug4 \
     --resume_from_checkpoint True \
     --ignore_data_skip \
     --num_beams 5 \
