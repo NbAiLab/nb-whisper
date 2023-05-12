9ac1a972aa25c98c475c3c2a880ccd113b6bf094
Per Egil Kummervold <per@capia.no>
2023-05-12 13:35:06 +0000

    loss experiment

--- run_nb_flax_speech_recognition_seq2seq_streaming_dev3.py
+++ run_nb_flax_speech_recognition_seq2seq_streaming_dev3.py
@@ -493,7 +493,6 @@ class FlaxDataCollatorSpeechSeq2SeqWithPadding:
         batch["decoder_input_ids"] = decoder_input_ids
         batch["attention_mask"] = labels_batch.attention_mask  # Add attention_mask to the batch
         
-        breakpoint()
         return batch
 
 
