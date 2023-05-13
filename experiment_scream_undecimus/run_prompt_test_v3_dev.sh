e79b0dc87487fce7870d0fbff57c1fe2f8284a2f
Per Egil Kummervold <per@capia.no>
2023-05-13 10:01:51 +0000

    loss experiment

--- experiment_scream_undecimus/run_prompt_test_v3_dev.sh
+++ experiment_scream_undecimus/run_prompt_test_v3_dev.sh
@@ -2,7 +2,7 @@ export TOKENIZERS_PARALLELISM=false
 export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-python ../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
+python ../run_nb_flax_speech_recognition_seq2seq_streaming_dev_pere.py \
         --model_name_or_path openai/whisper-small \
         --run_name "Scream - prompt test v3 dev" \
         --run_description "A Small Scream model. Prompt test 3 - dev." \
