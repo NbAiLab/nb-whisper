35440db86f8679c6e03399a3ed0783ad96cabaae
Per Egil Kummervold <per@capia.no>
2023-05-16 12:17:15 +0000

    updating training scripts, mappng

--- data_mapping_scream.py
+++ data_mapping_scream.py
@@ -1,19 +1,26 @@
 from typing import Union
 from datasets import Dataset, DatasetDict
+import string
 
-def transform_text_to_xxx(dataset: Dataset) -> Dataset:
-    return dataset.map(lambda example: {'text': "xxx"})
-
-def add_yyy_to_source(dataset: Dataset) -> Dataset:
-    return dataset.map(lambda example: {'source': example['source'] + " yyy"})
+def make_style_tags(dataset: Dataset) -> Dataset:
+    def transform(example):
+        if example['source'] == 'NPSC' or example['source'] == 'Fleurs':
+            return {'prompt': ''}
+        elif example['verbosity'] <= 2 and example['source'] == 'NRK':
+            return {'prompt': '[rv]'}
+        elif example['verbosity'] >= 3 and example['source'] == 'NRK':
+            return {'prompt': '[nv]'}
+        elif example['source'] == 'NST':
+            text = example['text'].translate(str.maketrans('', '', string.punctuation)).lower()
+            return {'prompt': f'[fv]', 'text': f'[{text}]'}
+    
+    return dataset.map(transform)
 
 # Should now handle both datasets and dataset dictionaries
 def map_data(data: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
-    
     # Do the mapping
     def transform(dataset: Dataset) -> Dataset:
-        dataset = transform_text_to_xxx(dataset)
-        dataset = add_yyy_to_source(dataset)
+        dataset = make_style_tags(dataset)
         return dataset
 
     # Check if this is a dataset or a dataset dictionary
