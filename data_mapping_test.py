2044cf8aef83e5ee0f5660f25d2db80b55424d4f
Per Egil Kummervold <per@capia.no>
2023-05-16 08:01:59 +0000

    pere training script

--- data_mapping_test.py
+++ data_mapping_test.py
@@ -1,4 +1,5 @@
-from datasets import Dataset
+from typing import Union
+from datasets import Dataset, DatasetDict
 
 def transform_text_to_xxx(dataset: Dataset) -> Dataset:
     return dataset.map(lambda example: {'text': "xxx"})
@@ -6,7 +7,17 @@ def transform_text_to_xxx(dataset: Dataset) -> Dataset:
 def add_yyy_to_source(dataset: Dataset) -> Dataset:
     return dataset.map(lambda example: {'source': example['source'] + " yyy"})
 
-def map_data(dataset: Dataset) -> Dataset:
-    dataset = transform_text_to_xxx(dataset)
-    dataset = add_yyy_to_source(dataset)
-    return dataset
+# Should now handle both datasets and dataset dictionaries
+def map_data(data: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
+    
+    # Do the mapping
+    def transform(dataset: Dataset) -> Dataset:
+        dataset = transform_text_to_xxx(dataset)
+        dataset = add_yyy_to_source(dataset)
+        return dataset
+
+    # Check if this is a dataset or a dataset dictionary
+    if isinstance(data, dict):
+        return {key: transform(dataset) for key, dataset in data.items()}
+    else:
+        return transform(data)
