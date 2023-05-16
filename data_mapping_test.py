3b8835db996801668c61910515c6ef92a59dea07
Per Egil Kummervold <per@capia.no>
2023-05-16 07:36:27 +0000

    pere training script

--- data_mapping_test.py
+++ data_mapping_test.py
@@ -1,10 +1,10 @@
 from datasets import Dataset
 
 def transform_text_to_xxx(dataset: Dataset) -> Dataset:
-    return dataset.map(lambda example: {'text': "xxx"}, remove_columns=['text'])
+    return dataset.map(lambda example: {'text': "xxx"})
 
 def add_yyy_to_source(dataset: Dataset) -> Dataset:
-    return dataset.map(lambda example: {'source': example['source'] + " yyy"}, remove_columns=['source'])
+    return dataset.map(lambda example: {'source': example['source'] + " yyy"})
 
 def map_data(dataset: Dataset) -> Dataset:
     dataset = transform_text_to_xxx(dataset)
