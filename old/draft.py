f8dee7bd2581ff502412d3a1edf56bee62ed0319
Per Egil Kummervold <per@capia.no>
2023-05-25 11:55:49 +0000

    debug punctation training scripts

--- old/draft.py
+++ old/draft.py
@@ -3,8 +3,8 @@ import torchaudio
 
 
 
-model_name = "NbAiLab/scream_tertius_dropout_replicate_test7b";processor = WhisperProcessor.from_pretrained(model_name, cache_dir="prompt"); model = WhisperForConditionalGeneration.from_pretrained(model_name, cache_dir="prompt", from_flax=True)
-input_speech, sr = torchaudio.load("audio2.mp3");input_features = processor(input_speech.squeeze(), sampling_rate=sr, return_tensors="pt", from_flax=True).input_features
+model_name = "NbAiLab/scream_tertius_dropout_replicate_test7c_nodropout";processor = WhisperProcessor.from_pretrained(model_name, cache_dir="prompt"); model = WhisperForConditionalGeneration.from_pretrained(model_name, cache_dir="prompt", from_flax=True)
+input_speech, sr = torchaudio.load("audio.mp3");input_features = processor(input_speech.squeeze(), sampling_rate=sr, return_tensors="pt", from_flax=True).input_features
 
 # --- Without prompt ---
 output_without_prompt = model.generate(input_features, language="no");print(processor.decode(output_without_prompt[0], skip_special_tokens=False))
