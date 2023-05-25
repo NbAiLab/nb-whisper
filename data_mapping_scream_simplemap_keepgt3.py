e51c62f4276138aa048925a325163630ff69d9b7
Per Egil Kummervold <per@capia.no>
2023-05-25 19:39:03 +0000

    debug punctation training scripts

--- data_mapping_scream_simplemap_keepgt3.py
+++ data_mapping_scream_simplemap_keepgt3.py
@@ -2,21 +2,19 @@ from typing import Union
 from datasets import Dataset, DatasetDict
 import string
 
-
 def make_style_tags(example):
     if example['verbosity'] <= 3:
-        return None
+        return {**example, 'remove_example': True}
     elif example['source'] == 'Fleurs':
-        return {**example, 'prompt': ''}
+        return {**example, 'prompt': '', 'remove_example': False}
     elif example['verbosity'] == 6:
-        return {**example, 'prompt': ''}
+        return {**example, 'prompt': '', 'remove_example': False}
     else:
-        return {**example, 'prompt': '#'}
+        return {**example, 'prompt': '#', 'remove_example': False}
 
 # Should now handle both datasets and dataset dictionaries
 def map_data(data: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
-    # Check if this is a dataset or a dataset dictionary
     if isinstance(data, dict):
-        return {key: dataset.map(make_style_tags).filter(lambda x: x is not None) for key, dataset in data.items()}
+        return {key: dataset.map(make_style_tags).filter(lambda x: not x['remove_example']) for key, dataset in data.items()}
     else:
-        return data.map(make_style_tags).filter(lambda x: x is not None)
+        return data.map(make_style_tags).filter(lambda x: not x['remove_example'])
--- old/draft.py
+++ old/draft.py
@@ -4,7 +4,7 @@ import torchaudio
 
 
 model_name = "NbAiLab/scream_tertius_simplemap";processor = WhisperProcessor.from_pretrained(model_name, cache_dir="prompt"); model = WhisperForConditionalGeneration.from_pretrained(model_name, cache_dir="prompt", from_flax=True)
-input_speech, sr = torchaudio.load("audio.mp3");input_features = processor(input_speech.squeeze(), sampling_rate=sr, return_tensors="pt", from_flax=True).input_features
+input_speech, sr = torchaudio.load("audio6.mp3");input_features = processor(input_speech.squeeze(), sampling_rate=sr, return_tensors="pt", from_flax=True).input_features
 
 # --- Without prompt ---
 output_without_prompt = model.generate(input_features, language="no");print(processor.decode(output_without_prompt[0], skip_special_tokens=False))
