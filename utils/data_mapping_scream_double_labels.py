b9e17f9e2172392a633ac97073128886f1e84a67
Per Egil Kummervold <per@capia.no>
2023-05-27 10:30:54 +0000

    debug punctation training scripts

--- utils/data_mapping_scream_double_labels.py
+++ utils/data_mapping_scream_double_labels.py
@@ -1,25 +1,24 @@
 from typing import Union
-from datasets import Dataset, DatasetDict
-import pandas as pd
+from datasets import Dataset, DatasetDict, concatenate_datasets
 
 def make_style_tags(example):
     if example['source'] == 'NST':
-        return [{**example, 'prompt': ''}, {**example, 'prompt': '[READING]'}]
+        return [{'prompt': ''}, {'prompt': '[READING]'}]
     elif example['source'] == 'Fleurs':
-        return [{**example, 'prompt': ''}]
+        return [{'prompt': ''}]
     elif example['source'] == 'NPSC':
-        return [{**example, 'prompt': '[PROCEEDING]'}]
+        return [{'prompt': '[PROCEEDING]'}]
     elif example['source'] == 'NRK TV':
-        return [{**example, 'prompt': '[SUBTITLE]'}]
+        return [{'prompt': '[SUBTITLE]'}]
     else:
         print("There is potentially an error in the dataset. Please check the example below:")
         print(example)
-        return [{**example, 'prompt': ''}]
+        return [{'prompt': ''}]
 
 def map_data(data: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
     if isinstance(data, DatasetDict):
-        return DatasetDict({key: Dataset.from_pandas(pd.DataFrame(make_style_tags(dataset))) for key, dataset in data.items()})
+        return DatasetDict({key: concatenate_datasets([Dataset.from_dict(ex) for ex in dataset.map(make_style_tags, remove_columns=dataset.column_names)]) for key, dataset in data.items()})
     elif isinstance(data, Dataset):
-        return Dataset.from_pandas(pd.DataFrame(make_style_tags(data)))
+        return concatenate_datasets([Dataset.from_dict(ex) for ex in data.map(make_style_tags, remove_columns=data.column_names)])
     else:
         raise TypeError(f"Unsupported data type: {type(data)}. This function expects a Dataset or DatasetDict.")
