8b9ad806743672e6122527555626e3399ff4436a
Per Egil Kummervold <per@capia.no>
2023-05-30 06:23:57 +0000

    debug punctation training scripts

--- scream_experiments/experiment_scream_tertiusdecimus/run_simplemap_labels_proceeding_allminus100_bpedropout_activation_highlr.sh
+++ scream_experiments/experiment_scream_tertiusdecimus/run_simplemap_labels_proceeding_allminus100_bpedropout_activation_highlr.sh
@@ -13,7 +13,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --text_column_name text \
         --train_split_name train \
         --eval_split_name validation \
-        --output_dir ../../../scream_tertius_simplemap_labels_proceeding_allminus100_bpedropout_activation\
+        --output_dir ../../../scream_tertius_simplemap_labels_proceeding_allminus100_bpedropout_activation_highlr\
         --overwrite_output_dir\
         --warmup_steps 2000 \
         --do_train \
@@ -21,7 +21,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --num_train_steps 20000 \
         --lr_scheduler_type linear \
         --eval_steps 1000 \
-        --learning_rate 2e-5 \
+        --learning_rate 5e-5 \
         --preprocessing_num_workers 32 \
         --per_device_train_batch_size 32 \
         --per_device_eval_batch_size 4 \
@@ -32,7 +32,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --use_auth_token True \
         --dtype bfloat16 \
         --hub_private_repo True \
-        --hub_model_id NbAiLab/scream_tertius_simplemap_labels_proceeding_allminus100_bpedropout_activation\
+        --hub_model_id NbAiLab/scream_tertius_simplemap_labels_proceeding_allminus100_bpedropout_activation_highlr\
         --resume_from_checkpoint True \
         --num_beams 5 \
         --ignore_data_skip \
