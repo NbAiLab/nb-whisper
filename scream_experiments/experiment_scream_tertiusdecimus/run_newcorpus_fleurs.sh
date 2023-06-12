574e370b365880c5230b0837db3924c6b8ca4aed
Per Egil Kummervold <per@capia.no>
2023-06-12 11:47:10 +0000

    new corpus debugging

---
+++ scream_experiments/experiment_scream_tertiusdecimus/run_newcorpus_fleurs.sh
@@ -0,0 +1,47 @@
+export TOKENIZERS_PARALLELISM=false
+export CMALLOC_VERBOSE=0
+export TCMALLOC_VERBOSE=0
+export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
+python ../../run_nb_flax_speech_recognition_seq2seq_streaming_debug2.py \
+        --model_name_or_path openai/whisper-small \
+        --run_name "Scream - tertius_simplemap_labels bpe and activation" \
+        --run_description "A Small Scream model. Labels" \
+        --wandb_entity "nbailab" \
+        --wandb_project "Scream - duodecimus" \
+        --dataset_name NbAiLab/ncc_speech \
+        --language Norwegian \
+        --text_column_name text \
+        --train_split_name train \
+        --eval_split_name validation \
+        --output_dir ../../../scream_tertiusdecimus_newcopus_fleurs\
+        --overwrite_output_dir\
+        --warmup_steps 2000 \
+        --do_train \
+        --do_eval \
+        --do_predict False \
+        --num_train_steps 20000 \
+        --lr_scheduler_type linear \
+        --eval_steps 1000 \
+        --learning_rate 5e-5 \
+        --preprocessing_num_workers 32 \
+        --per_device_train_batch_size 32 \
+        --per_device_eval_batch_size 4 \
+        --predict_with_generate \
+        --log_max_eval_predictions 50 \
+        --log_eval_predictions_fn "utils.log_predictions.write_predictions" \
+        --streaming True \
+        --use_auth_token True \
+        --dtype bfloat16 \
+        --hub_private_repo True \
+        --hub_model_id NbAiLab/scream_tertiusdecimus_newcopus_fleurs\
+        --resume_from_checkpoint True \
+        --num_beams 5 \
+        --ignore_data_skip \
+        --gradient_checkpointing True \
+        --prev_column_name "prompt" \
+        --log_examples 100 \
+        --data_mapping_fn "utils.data_mapping_scream_labels_proceeding_newds.map_data" \
+        --bpe_dropout 0.1 \
+        --activation_dropout 0.1 \
+        --push_to_hub
+        
