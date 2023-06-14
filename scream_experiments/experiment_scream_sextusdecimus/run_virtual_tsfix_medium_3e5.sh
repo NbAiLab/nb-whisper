4e1ad4776c02a17b6d300d5ff6d9aabdac59fadd
Per Egil Kummervold <per@capia.no>
2023-06-14 05:26:27 +0000

    new experiments

--- scream_experiments/experiment_scream_sextusdecimus/run_virtual_tsfix_medium_3e5.sh
+++ scream_experiments/experiment_scream_sextusdecimus/run_virtual_tsfix_medium_3e5.sh
@@ -3,9 +3,9 @@ export CMALLOC_VERBOSE=0
 export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
-        --model_name_or_path openai/whisper-small \
+        --model_name_or_path openai/whisper-medium \
         --run_name "Scream - scream_sextusdecimus_virtual_tsfix_medium" \
-        --run_description "A Small Scream model. Labels" \
+        --run_description "A Medium Scream model. Labels" \
         --wandb_entity "nbailab" \
         --wandb_project "Scream - duodecimus" \
         --dataset_name NbAiLab/ncc_speech \
@@ -14,7 +14,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --train_split_name train \
         --dataset_load_fn "utils.dataset_load_scream.load_dataset_scream" \
         --eval_split_name validation \
-        --output_dir ../../../scream_sextusdecimus_virtual_tsfix_medium_lowlr\
+        --output_dir ../../../scream_sextusdecimus_virtual_tsfix_medium_3e5 \
         --overwrite_output_dir\
         --warmup_steps 2000 \
         --do_train \
@@ -22,7 +22,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --num_train_steps 20000 \
         --lr_scheduler_type linear \
         --eval_steps 1000 \
-        --learning_rate 2e-5 \
+        --learning_rate 3e-5 \
         --preprocessing_num_workers 32 \
         --per_device_train_batch_size 32 \
         --per_device_eval_batch_size 4 \
@@ -33,7 +33,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
         --use_auth_token True \
         --dtype bfloat16 \
         --hub_private_repo True \
-        --hub_model_id NbAiLab/scream_sextusdecimus_virtual_tsfix_medium_lowlr \
+        --hub_model_id NbAiLab/scream_sextusdecimus_virtual_tsfix_medium_3e5 \
         --resume_from_checkpoint True \
         --num_beams 5 \
         --ignore_data_skip \
