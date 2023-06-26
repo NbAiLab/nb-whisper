0194b3f330bbcedf3cddc599877e517cb93e9b2b
Per Egil Kummervold <per@capia.no>
2023-06-26 21:42:11 +0000

    fixing audio book bug

--- utils/dataset_load_scream_test.py
+++ utils/dataset_load_scream_test.py
@@ -3,7 +3,7 @@ import datasets
 
 
 def processor_normal(sample):
-    if sample["source"].lower() not in ("nrk_tv", "nrk_tv_translate", "audio_books"):
+    if sample["source"].lower() not in ("nrk_tv", "nrk_tv_translate"):
         return {**sample, "previous_text": None, "timestamped_text": None}
 
 
@@ -31,13 +31,13 @@ def processor_previous_text_prompts(sample):
 
 def processor_style_prompts(sample):
     mapping = {
-        "nrk_tv": "<|startoflm|>",
-        "nrk_tv_translate": "<|startoflm|>",
-        # "npsc": "",
-        # "stortinget": "",
-        # "nst": "",
-        # "fleurs": "",
-        # "audio_books": "",
+        "nrk_tv": "[SUBTITLE]",
+        "nrk_tv_translate": "[SUBTITLE]",
+        "npsc": "[REPORT]",
+        "stortinget": "[REPORT]",
+        "nst": "[VERBATIM]",
+        "fleurs": "[VERBATIM]",
+        "audio_book": "[VERBATIM]",
     }
     if sample["source"].lower() in mapping.keys():
         return {**sample, "previous_text": mapping.get(sample["source"].lower(), ""), "timestamped_text": None}
@@ -45,13 +45,13 @@ def processor_style_prompts(sample):
 
 def processor_previous_text_style_prompts(sample):
     mapping = {
-        "nrk_tv": "<|startoflm|>",
-        "nrk_tv_translate": "<|startoflm|>",
-        # "npsc": "",
-        # "stortinget": "",
-        # "nst": "",
-        # "fleurs": "",
-        # "audio_books": "",
+        "nrk_tv": "[SUBTITLE]",
+        "nrk_tv_translate": "[SUBTITLE]",
+        "npsc": "[REPORT]",
+        "stortinget": "[REPORT]",
+        "nst": "[VERBATIM]",
+        "fleurs": "[VERBATIM]",
+        "audio_book": "[VERBATIM]",
     }
     if sample["previous_text"] is not None and sample["source"].lower() in mapping.keys():
         return {
