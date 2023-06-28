f966f87a77a581936ad74cdc94f2fab666282372
Per Egil Kummervold <per@capia.no>
2023-06-28 05:06:33 +0000

    more experiments

--- scream_experiments/public_beta/run_small.sh
+++ scream_experiments/public_beta/run_small.sh
@@ -15,7 +15,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train \
-    --dataset_load_fn "utils.dataset_load_startoflm_default_nonverbatim.load_dataset_scream" \
+    --dataset_load_fn "utils.dataset_load_scream_startoflm_default_nonverbatim.load_dataset_scream" \
     --test_split_name "test_fleurs,test_stortinget,test_nrk_tv,test_audio_books" \
     --eval_split_name "validation_fleurs,validation_stortinget,validation_nrk_tv,validation_audio_books" \
     --hub_model_id NbAiLab/nb-whisper_public_beta_small_testrun2 \
