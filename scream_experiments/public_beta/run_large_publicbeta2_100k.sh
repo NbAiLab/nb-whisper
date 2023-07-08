475d9856c57018ed534d5ff0ad05a2efa6e903ed
Per Egil Kummervold <per@capia.no>
2023-07-08 09:21:25 +0000

    test

--- scream_experiments/public_beta/run_large_publicbeta2_100k.sh
+++ scream_experiments/public_beta/run_large_publicbeta2_100k.sh
@@ -18,9 +18,8 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --dataset_load_fn "utils.dataset_load_public_beta.load_dataset_scream" \
     --test_split_name "test_fleurs,test_stortinget" \
     --eval_split_name "validation_fleurs,validation_stortinget" \
-    --hub_model_id NbAiLab/nb-whisper-medium-mytest-100k \
-    --output_dir ../../../nb-whisper-largetest-100k \
-    --output_dir ../../../nb-whisper-largetest-100k \
+    --hub_model_id NbAiLab/nb-whisper-largetest2-100k \
+    --output_dir ../../../nb-whisper-largetest2-100k \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
