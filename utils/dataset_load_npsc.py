9c738cf0b5758bab91a4b502e3582fbbb0656b63
Per Egil Kummervold <per@capia.no>
2023-07-20 11:42:44 +0000

    update script npsc

--- utils/dataset_load_npsc.py
+++ utils/dataset_load_npsc.py
@@ -1,39 +1,37 @@
 import random
 import datasets
 import string
+import re
+
+def processor_general(sample, field):
+    # Lowercase the text
+    sample[field] = sample[field].lower()
+    
+    # Remove punctuation
+    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) # map punctuation to space
+    sample[field] = sample[field].translate(translator)
+    
+    # Remove all words between "<" and ">"
+    sample[field] = re.sub('<.*?>', '', sample[field])
+
+    # Remove double spacing
+    sample[field] = ' '.join(sample[field].split())
+
+    return sample
 
 def processor_normal_norm(sample):
-    # Lowercase the text
-    sample["normsentence_text"] = sample["normsentence_text"].lower()
-    
-    # Remove punctuation
-    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) # map punctuation to space
-    sample["normsentence_text"] = sample["normsentence_text"].translate(translator)
-    
-    # Remove double spacing
-    sample["normsentence_text"] = ' '.join(sample["normsentence_text"].split())
-
-    return sample
+    return processor_general(sample, "normsentence_text")
 
 def processor_normal(sample):
-    # Lowercase the text
-    sample["text"] = sample["text"].lower()
-    
-    # Remove punctuation
-    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) # map punctuation to space
-    sample["text"] = sample["text"].translate(translator)
-    
-    # Remove double spacing
-    sample["text"] = ' '.join(sample["text"].split())
-
-    return sample
+    return processor_general(sample, "text")
 
 def load_dataset_npsc_norm(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
-    processors = [processor_normal_norm]
-    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, post_processors=processors, **kwargs)
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, **kwargs)
+    ds = ds.map(processor_normal_norm)
     return ds
 
 def load_dataset_npsc(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
-    processors = [processor_normal]
-    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, post_processors=processors, **kwargs)
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, **kwargs)
+    ds = ds.map(processor_normal)
     return ds
+
