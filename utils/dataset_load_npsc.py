b9fd2f2bc622a947cbbcb263be968bf8fa52645e
Per Egil Kummervold <per@capia.no>
2023-07-29 12:03:19 +0000

    update train bash script npsc

--- utils/dataset_load_npsc.py
+++ utils/dataset_load_npsc.py
@@ -3,17 +3,19 @@ import datasets
 import string
 import re
 
-def processor_general(sample, field):
+def processor_general(sample, field, remove_hesitations=False):
     # Lowercase the text
     sample[field] = sample[field].lower()
     
-    # Remove all words between "<" and ">"
-    sample[field] = re.sub('<.*?>', '', sample[field])
+    if remove_hesitations == True:
+        # Remove all words between "<" and ">"
+        sample[field] = re.sub('<.*?>', '', sample[field])
 
     # Remove punctuation unless it's following a digit
+    # This will not affect the non-normalized text, since they have no digits
     sample[field] = re.sub(r'(?<!\d)[.,;!?](?!\d)', ' ', sample[field])
 
-    # Replace "tunell" with "tunnel"
+    # Replace "tunell" with "tunnel". NPSC is following a different standard of spelling here
     sample[field] = sample[field].replace('tunell', 'tunnel')
 
     # Remove double spacing
@@ -21,23 +23,22 @@ def processor_general(sample, field):
 
     return sample
 
-
-def processor_normal_norm(sample):
-    return processor_general(sample, "normsentence_text")
-
-def processor_normal(sample):
-    return processor_general(sample, "text")
-
-def load_dataset_npsc_norm(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
-    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, **kwargs)
-    ds = ds.map(processor_normal_norm)
-    ds = ds.filter(lambda example: '<inaudible>' not in example['normsentence_text'])
+def load_dataset_npsc(dataset_name, field, remove_hesitations=False, dataset_config_name=None, split="train", streaming=True, cache_dir=None, use_auth_token=None):
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, cache_dir=cache_dir, streaming=streaming, use_auth_token=use_auth_token)
+    ds = ds.map(lambda sample: processor_general(sample, field, remove_hesitations))
+    ds = ds.filter(lambda example: '<inaudible>' not in example[field])
     return ds
 
-def load_dataset_npsc(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
-    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, **kwargs)
-    ds = ds.map(processor_normal)
-    ds = ds.filter(lambda example: '<inaudible>' not in example['text'])
-    return ds
+def load_dataset_npsc_raw(dataset_name, dataset_config_name=None, split="train", streaming=True, cache_dir=None, use_auth_token=None):
+    return load_dataset_npsc(dataset_name, 'text', False, dataset_config_name, split, streaming, cache_dir, use_auth_token)
+
+def load_dataset_npsc_nohes(dataset_name, dataset_config_name=None, split="train", streaming=True, cache_dir=None, use_auth_token=None):
+    return load_dataset_npsc(dataset_name, 'text', True, dataset_config_name, split, streaming, cache_dir, use_auth_token)
+
+def load_dataset_npsc_norm_raw(dataset_name, dataset_config_name=None, split="train", streaming=True, cache_dir=None, use_auth_token=None):
+    return load_dataset_npsc(dataset_name, 'normsentence_text', False, dataset_config_name, split, streaming, cache_dir, use_auth_token)
+
+def load_dataset_npsc_norm_nohes(dataset_name, dataset_config_name=None, split="train", streaming=True, cache_dir=None, use_auth_token=None):
+    return load_dataset_npsc(dataset_name, 'normsentence_text', True, dataset_config_name, split, streaming, cache_dir, use_auth_token)
 
 
