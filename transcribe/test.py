8627943640276b084a3b20ec0069e51fe3286380
Per Egil Kummervold <per@capia.no>
2023-07-30 21:07:07 +0000

    update train bash script fine

--- transcribe/test.py
+++ transcribe/test.py
@@ -4,7 +4,6 @@ from flax.jax_utils import replicate
 from flax.training.common_utils import shard
 from jax import device_get, pmap
 from transformers import WhisperProcessor
-
 from whisper_jax import FlaxWhisperForConditionalGeneration
 
 # load the processor and model
@@ -29,21 +28,21 @@ ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="v
 sample = ds[0]["audio"]
 
 # pre-process: convert the audio array to log-mel input features
-# Assume input_features_single is a single example of shape (80, 3000)
+# Assume input_features_single is a single example of shape (1, 80, 3000)
 input_features_single = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="np").input_features
 
 # Stack 4 identical copies to create a batch
 input_features = jnp.stack([input_features_single] * 4)
-input_features = input_features.squeeze()
 
-
+# Make sure input features is of shape (4, 1, 80, 3000)
+print(input_features.shape)  # it should print: (4, 1, 80, 3000)
 
 # replicate the input features across devices for DP
-print(input_features.shape)
 input_features = shard(input_features)
+
 # run the forward pass (JIT compiled the first time it is called)
 pred_ids = p_generate(input_features)
 output_ids = device_get(pred_ids.reshape(-1, model.config.max_length))
 
 # post-process: convert tokens ids to text string
-transcription = processor.batch_decode(pred_ids, skip_special_tokens=True)
+transcription = processor.batch_decode(output_ids, skip_special_tokens=True)
