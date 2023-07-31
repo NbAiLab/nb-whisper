0de5a842b40a27bed5d925722c4be98d31f0970a
Per Egil Kummervold <per@capia.no>
2023-07-31 14:08:43 +0000

    update train bash script fine

--- transcribe/transcribe_pipeline_nobatch.py
+++ transcribe/transcribe_pipeline_nobatch.py
@@ -8,18 +8,19 @@ from whisper_jax import FlaxWhisperPipline
 librispeech = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
 
 
-eval_dataloader = librispeech.with_format("numpy").iter(batch_size=1)
+eval_dataloader = librispeech.with_format("numpy").iter(batch_size=4)
 
 # Create the FlaxWhisperPipeline object
 pipeline = FlaxWhisperPipline(
     "openai/whisper-tiny.en",
     dtype=jnp.bfloat16,  # use bfloat16 precision
+    batch_size=4,  # enable batching
 )
 
 while True:
     audio = next(iter(eval_dataloader))
     start = time.time()
-    transcription = pipeline(audio["audio"][0]["path"])
+    transcription = pipeline(audio["audio"][0:4]["path"])
     runtime = time.time() - start
     print(f"{runtime:.06}")
 
