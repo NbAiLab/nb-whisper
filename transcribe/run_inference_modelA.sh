5c938c9df4f555899c5795f6b5af679b0e69c298
Per Egil Kummervold <per@capia.no>
2023-09-20 10:51:55 +0000

    inference

--- transcribe/run_inference_modelA.sh
+++ transcribe/run_inference_modelA.sh
@@ -3,7 +3,7 @@
 MODEL_NAME="NbAiLab/nb-whisper-medium-fine4-npsc-norm-nohes"
 OUTPUT_DIR="./ModelA"
 WANDB_PROJECT="nb-whisper-public-beta-transcription"
-BATCH_SIZE=144
+BATCH_SIZE=192
 NUM_BEAMS=1
 #BATCH_SIZE=32
 #NUM_BEAMS=3
