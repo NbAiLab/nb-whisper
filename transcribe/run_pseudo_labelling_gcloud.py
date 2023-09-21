b22a653b46cfede304f9b2bb17793837af2729cb
Per Egil Kummervold <per@capia.no>
2023-09-21 18:45:49 +0000

    inference

--- transcribe/run_pseudo_labelling_gcloud.py
+++ transcribe/run_pseudo_labelling_gcloud.py
@@ -814,19 +814,9 @@ def main():
                 model_name = model_args.model_name_or_path.replace("/", "-")
                 output_csv = os.path.join(output_dir, f"step{step}-{model_name}-{data_args.language}-{data_args.task}-{split}-transcription.tsv")
 
-                
-                eval_preds_list = [arr.tolist() for arr in eval_preds]
-                start_time_1 = time.time()
+                eval_preds_list = [arr.tolist() for arr in eval_preds] 
                 pred_str = tokenizer.batch_decode(eval_preds_list, skip_special_tokens=True)
-                end_time_1 = time.time() - start_time_1
-
-                start_time_2 = time.time()
-                pred_str2 = [tokenizer.decode(t, skip_special_tokens=True) for t in eval_preds_list]
-                end_time_2 = time.time() - start_time_2
-               
                 
-               
-
                 logger.info(f"Tokenizer time for tokenizing {len(eval_preds)}: {end_time_1} vs {end_time_2} seconds")
 
                 csv_data = [[eval_ids[i], pred_str[i]] for i in range(len(pred_str))]
@@ -853,9 +843,9 @@ def main():
                     # Copy file to Google Cloud Storage
                     gcs_path = os.path.join(GCS_BUCKET, output_csv)
                     copy_command = f"gsutil cp {output_csv} {gcs_path}"
-                    #result = os.system(copy_command)
+                    result = os.system(copy_command)
                     
-                    logger.info(f"Result of gcs copy: {copy_command}")
+                    logger.info(f"Result of gcs copy: {result}")
 
 
         eval_time = time.time() - eval_start
