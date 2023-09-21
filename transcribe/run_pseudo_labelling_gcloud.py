6277058e484ef5a33f59ffae0b3bbc4f04e02953
Per Egil Kummervold <per@capia.no>
2023-09-21 10:30:01 +0000

    inference

--- transcribe/run_pseudo_labelling_gcloud.py
+++ transcribe/run_pseudo_labelling_gcloud.py
@@ -798,6 +798,7 @@ def main():
                         blocking=False,
                     )
                 else:
+                    x=3
                     logger.info("Here we should push to the bucket")
 
         eval_time = time.time() - eval_start
