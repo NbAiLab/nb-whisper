1abffd2e2ffa5902383977870fcb35863cdf20e4
Per Egil Kummervold <per@capia.no>
2023-09-21 10:37:28 +0000

    inference

--- transcribe/run_pseudo_labelling_gcloud.py
+++ transcribe/run_pseudo_labelling_gcloud.py
@@ -798,9 +798,9 @@ def main():
                         blocking=False,
                     )
                 else:
-                    logger.info("Here we should push to the bucket")
+                    # logger.info("Here we should push to the bucket")
                     x = 5
-                    logger.info("Here we should push to the bucket")
+                    # logger.info("Here we should push to the bucket")
 
 
 
