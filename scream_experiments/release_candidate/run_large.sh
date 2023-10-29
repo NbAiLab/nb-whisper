f20f79848481b32e289746aaa13a1b20ce5c979c
Per Egil Kummervold <per@capia.no>
2023-10-29 14:22:35 +0000

    even smaller

--- scream_experiments/release_candidate/run_large.sh
+++ scream_experiments/release_candidate/run_large.sh
@@ -35,8 +35,9 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --adam_beta2 0.98 \
     --adam_epsilon 1e-6 \
     --bpe_dropout 0.2 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --attention_dropout 0.1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --preprocessing_num_workers 8 \
     --timestamp_column_name "timestamped_text" \
     --prev_column_name "previous_text" \
