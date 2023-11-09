a4e15b4d35a6f63640b296a479b4592036fab684
Per Egil Kummervold <per@capia.no>
2023-11-09 08:49:14 +0000

    finetune test1

--- scream_experiments/release_candidate/run_small_finetune_test1.sh
+++ scream_experiments/release_candidate/run_small_finetune_test1.sh
@@ -5,17 +5,17 @@ export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 
 # Running the Python script
-python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev_no.py \
+python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --model_name_or_path NbAiLab/nb-whisper-small-RC1 \
     --run_name "NB-Whisper - small test1" \
     --run_description "A small NB-Whisper RC1" \
     --wandb_entity "nbailab" \
     --wandb_project "NB-Whisper RC1" \
-    --dataset_name NbAiLab/ncc_speech_v7 \
+    --dataset_name NbAiLab/ncc_speech_styling_v1 \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train \
-    --dataset_load_fn "utils.dataset_load_rc.load_dataset_nbwhisper_finetune" \
+    --dataset_load_fn "utils.dataset_load_rc_finetune.load_dataset_nbwhisper_finetune" \
     --test_split_name "test_norwegian_fleurs,test_nst,test_clean_audio_books_no" \
     --eval_split_name "validation_norwegian_fleurs,validation_nst,validation_clean_audio_books_no" \
     --hub_model_id NbAiLab/nb-whisper-small-RC1-finetune-test1 \
