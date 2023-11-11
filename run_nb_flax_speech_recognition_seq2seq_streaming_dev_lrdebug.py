d05731d85c98408c98d8b476db462f1960c60696
Per Egil Kummervold <per@capia.no>
2023-11-11 21:46:49 +0000

    test

--- run_nb_flax_speech_recognition_seq2seq_streaming_dev_lrdebug.py
+++ run_nb_flax_speech_recognition_seq2seq_streaming_dev_lrdebug.py
@@ -1740,7 +1740,9 @@ def main():
 
         # ========================== Evaluating ==========================
         # Evaluate at each eval_steps, and at the end of training at num_train_steps
-        if training_args.do_eval and (step % training_args.eval_steps == 0 or step == data_args.num_train_steps - 1):
+        if training_args.do_eval and step > 0 and (step % training_args.eval_steps == 0 or step == data_args.num_train_steps - 1):
+
+        #if training_args.do_eval and (step % training_args.eval_steps == 0 or step == data_args.num_train_steps - 1):
             logger.info(
                 f"Starting evaluation at step {step} of num_training_step {data_args.num_train_steps} steps. Planned evaluation every {training_args.eval_steps} steps." 
             )
