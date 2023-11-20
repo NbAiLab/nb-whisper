4f60ed045fba87017bb3d30a01cd2a037efa9055
Per Egil Kummervold <per@capia.no>
2023-11-20 22:32:35 +0000

    fix error in verbose scripts

--- scream_experiments/style_finetuning2/run_singletask_semantic_20k.sh
+++ scream_experiments/style_finetuning2/run_singletask_semantic_20k.sh
@@ -4,6 +4,9 @@ export CMALLOC_VERBOSE=0
 export TCMALLOC_semantic=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 
+
+NOT READY YET!!
+
 # Running the Python script
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --model_name_or_path NbAiLab/nb-whisper-tiny-RC1 \
@@ -11,22 +14,22 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --run_description "A NB-Whisper singletask_semantic_v3" \
     --wandb_entity "nbailab" \
     --wandb_project "NB-Whisper semantic tests" \
-    --dataset_name NbAiLab/ncc_speech_styling_v1 \
+    --dataset_name NbAiLab/ncc_speech_styling_v3 \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train \
     --dataset_load_fn "utils.dataset_load_rc_semantic.load_dataset_nbwhisper_rc_semantic" \
     --test_split_name "test_audio_books_no,test_norwegian_fleurs,test_nrk_no,test_nst,test_stortinget_no,test_clean_audio_books_no,test_clean_stortinget_no" \
     --eval_split_name "validation_audio_books_no,validation_norwegian_fleurs,validation_nrk_no,validation_nst,validation_stortinget_no,validation_clean_audio_books_no,validation_clean_stortinget_no" \
-    --hub_model_id NbAiLab/nb-whisper-tiny-singletask_rc_semantic_v3 \
-    --output_dir ../../../nb-whisper-tiny-singletask_rc_semantic_v3 \
+    --hub_model_id NbAiLab/nb-whisper-tiny-singletask_semantic_20k \
+    --output_dir ../../../nb-whisper-tiny-singletask_semantic_20k \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
     --do_eval \
     --predict_with_generate \
     --warmup_steps 1000 \
-    --num_train_steps 10000 \
+    --num_train_steps 20000 \
     --eval_steps 2000 \
     --lr_scheduler_type linear \
     --learning_rate 1e-5 \
