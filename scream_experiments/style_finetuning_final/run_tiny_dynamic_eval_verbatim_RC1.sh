d143ed56985ec9ac617dbcd4c723603c65256ef8
Per Egil Kummervold <per@capia.no>
2023-11-21 15:50:35 +0000

    dynamic scripts

--- scream_experiments/style_finetuning_final/run_tiny_dynamic_eval_verbatim_RC1.sh
+++ scream_experiments/style_finetuning_final/run_tiny_dynamic_eval_verbatim_RC1.sh
@@ -1,36 +1,33 @@
 # Environment variables
 export TOKENIZERS_PARALLELISM=false
 export CMALLOC_VERBOSE=0
-export TCMALLOC_semantic=0
+export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
-
-
-NOT READY YET!!
 
 # Running the Python script
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --model_name_or_path NbAiLab/nb-whisper-tiny-RC1 \
-    --run_name "NB-Whisper - tiny - singletask semantic_v3" \
-    --run_description "A NB-Whisper singletask_semantic_v3" \
+    --run_name "NB-Whisper - tiny - dynamic eval verbatim RC1" \
+    --run_description "A NB-Whisper dynamic eval verbatim RC1" \
     --wandb_entity "nbailab" \
-    --wandb_project "NB-Whisper semantic tests" \
-    --dataset_name NbAiLab/ncc_speech_styling_v3 \
+    --wandb_project "NB-Whisper dynamic tests" \
+    --dataset_name NbAiLab/ncc_speech_styling_v2 \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train \
-    --dataset_load_fn "utils.dataset_load_rc_semantic.load_dataset_nbwhisper_rc_semantic" \
+    --dataset_load_fn "utils.dataset_load_rc_dynamic.load_dataset_nbwhisper_rc_dynamic_eval_verbatim" \
     --test_split_name "test_audio_books_no,test_norwegian_fleurs,test_nrk_no,test_nst,test_stortinget_no,test_clean_audio_books_no,test_clean_stortinget_no" \
     --eval_split_name "validation_audio_books_no,validation_norwegian_fleurs,validation_nrk_no,validation_nst,validation_stortinget_no,validation_clean_audio_books_no,validation_clean_stortinget_no" \
-    --hub_model_id NbAiLab/nb-whisper-tiny-singletask_semantic_20k \
-    --output_dir ../../../nb-whisper-tiny-singletask_semantic_20k \
+    --hub_model_id NbAiLab/nb-whisper-tiny-dynamic-eval-verbatim-RC1 \
+    --output_dir ../../../nb-whisper-tiny-dynamic-eval-verbatim-RC1 \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
     --do_eval \
     --predict_with_generate \
     --warmup_steps 1000 \
-    --num_train_steps 20000 \
-    --eval_steps 2000 \
+    --num_train_steps 40000 \
+    --eval_steps 5000 \
     --lr_scheduler_type linear \
     --learning_rate 1e-5 \
     --weight_decay 0.01 \
@@ -39,7 +36,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --adam_epsilon 1e-6 \
     --bpe_dropout 0.2 \
     --activation_dropout 0.1 \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 8 \
     --preprocessing_num_workers 32 \
     --timestamp_column_name "timestamped_text" \
