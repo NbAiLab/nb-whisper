235bd37a13f80394114055432e409118a29fd345
Per Egil Kummervold <per@capia.no>
2023-11-24 15:45:31 +0000

    RC8 dynamic

---
+++ utils/dataset_load_rc_dynamic_downsampled.py
@@ -0,0 +1,125 @@
+import random
+import datasets
+
+def processor_test(sample):
+    if sample["source"] == "nrk_tv" or sample["source"] == "nrk_tv_silence" or sample["source"] == "nrk_translate" or sample["source"] == "stortinget":
+        return {**sample, "previous_text": None, "timestamped_text": None, "task": "translate"}
+    else:
+        return {**sample, "previous_text": None, "timestamped_text": None, "task": "transcribe"}
+
+def processor_test_verbatim(sample):
+    if sample:
+        return {**sample, "previous_text": None, "timestamped_text": None, "task": "transcribe"}
+
+def processor_test_semantic(sample):
+    if sample:
+        return {**sample, "previous_text": None, "timestamped_text": None, "task": "translate"}
+
+def downsample_wrapper(processor_function):
+    """
+    Wrapper function that applies downsampling to samples with task 'translate' after the primary processor function.
+    """
+    def wrapped_processor(sample):
+        processed_sample = processor_function(sample)
+        if processed_sample and processed_sample.get("task") == "translate" and random.random() < 0.5:
+            return None
+        return processed_sample
+    return wrapped_processor
+
+
+def processor_normal(sample):
+    return {**sample, "previous_text": None, "timestamped_text": None}
+
+def processor_normal_no(sample):
+    sample = processor_normal(sample)
+    if sample and sample["language"] == "no":
+        return sample
+
+def processor_normal_nn(sample):
+    sample = processor_normal(sample)
+    if sample and sample["language"] == "nn":
+        return sample
+
+def processor_normal_en(sample):
+    sample = processor_normal(sample)
+    if sample and sample["text_en"] is not None and sample["source"] != "audio_books":
+        return {
+            **sample,
+            "text": sample["text_en"],
+            "language": "en",
+            "previous_text": None,
+            "timestamped_text": None
+        }
+
+def processor_timestamps(sample):
+    if sample["timestamped_text"] is not None:
+        return {**sample, "text": sample["timestamped_text"], "previous_text": None}
+
+def processor_timestamps_en(sample):
+    if sample["timestamped_text_en"] is not None and sample["source"] != "audio_books":
+        return {**sample, "text": sample["timestamped_text_en"], "language": "en","timestamped_text": sample["timestamped_text_en"], "previous_text": None}
+
+def processor_previous_text_prompts(sample):
+    if sample["previous_text"] is not None:
+        return {**sample, "timestamped_text": None}
+
+def processor_timestamps_previous_text(sample):
+    if sample["previous_text"] is not None and sample["timestamped_text"] is not None:
+        return {**sample, "text": sample["timestamped_text"]}
+  
+def load_dataset_nbwhisper_rc_dynamic(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
+    processors = [
+        processor_normal_no,
+        processor_normal_nn,
+        processor_normal_en,
+        processor_timestamps,
+        processor_timestamps_en,
+        processor_previous_text_prompts,
+        processor_timestamps_previous_text,
+    ]
+
+    if split == "train":
+        processors = [downsample_wrapper(proc) for proc in processors]
+    else:
+        processors = [processor_test]
+        
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, post_processors=processors, **kwargs)
+    return ds
+
+def load_dataset_nbwhisper_rc_dynamic_eval_verbatim(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
+    processors = [
+        processor_normal_no,
+        processor_normal_nn,
+        processor_normal_en,
+        processor_timestamps,
+        processor_timestamps_en,
+        processor_previous_text_prompts,
+        processor_timestamps_previous_text,
+    ]
+
+    if split == "train":
+        processors = [downsample_wrapper(proc) for proc in processors]
+    else:
+        processors = [processor_test_verbatim]
+        
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, post_processors=processors, **kwargs)
+    return ds
+
+def load_dataset_nbwhisper_rc_dynamic_eval_semantic(dataset_name, dataset_config_name=None, split="train", streaming=True, **kwargs):
+    processors = [
+        processor_normal_no,
+        processor_normal_nn,
+        processor_normal_en,
+        processor_timestamps,
+        processor_timestamps_en,
+        processor_previous_text_prompts,
+        processor_timestamps_previous_text,
+    ]
+
+    if split == "train":
+        processors = [downsample_wrapper(proc) for proc in processors]
+    else:
+        processors = [processor_test_semantic]
+        
+    ds = datasets.load_dataset(dataset_name, dataset_config_name, split=split, streaming=streaming, post_processors=processors, **kwargs)
+    return ds
