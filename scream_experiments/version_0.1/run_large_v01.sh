decb274da49a68686e4a40ca24aa9f84e52388b7
Per Egil Kummervold <per@capia.no>
2023-11-28 08:50:14 +0000

    version 0.1

--- scream_experiments/version_0.1/run_large_v01.sh
+++ scream_experiments/version_0.1/run_large_v01.sh
@@ -5,49 +5,48 @@ export TCMALLOC_VERBOSE=0
 export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 
 # Running the Python script
-python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev_v3test.py \
-    --model_name_or_path openai/whisper-large-v2 \
-    --run_name "NB-Whisper - large memtest pere" \
-    --run_description "A large NB-Whisper memtest_pere" \
+python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
+    --model_name_or_path NbAiLab/nb-whisper-large-v3-RC4 \
+    --run_name "NB-Whisper - large - dynamic eval v0.1" \
+    --run_description "A NB-Whisper dynamic eval v0.1" \
     --wandb_entity "nbailab" \
-    --wandb_project "NB-Whisper" \
-    --dataset_name NbAiLab/ncc_speech_v7 \
+    --wandb_project "NB-Whisper dynamic v0.1" \
+    --dataset_name NbAiLab/ncc_speech_styling_v2 \
     --language Norwegian \
     --text_column_name text \
     --train_split_name train \
-    --dataset_load_fn "utils.dataset_load_rc.load_dataset_nbwhisper" \
-    --test_split_name "test_norwegian_fleurs" \
-    --eval_split_name "validation_norwegian_fleurs" \
-    --hub_model_id NbAiLab/nb-whisper-large-memtest_pere \
-    --output_dir ../../../nb-whisper-large-memtest_pere \
+    --dataset_load_fn "utils.dataset_load_rc_dynamic_noellipses.load_dataset_nbwhisper_rc_dynamic" \
+    --test_split_name "test_nst,test_clean_stortinget_no" \
+    --eval_split_name "validation_nst,validation_clean_stortinget_no" \
+    --hub_model_id NbAiLab/nb-whisper-large-v0.1 \
+    --output_dir ../../../nb-whisper-large-v0.1 \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
     --do_eval \
     --predict_with_generate \
-    --warmup_steps 50 \
-    --num_train_steps 5000 \
-    --eval_steps 500 \
+    --warmup_steps 1000 \
+    --num_train_steps 10000 \
+    --eval_steps 1000 \
     --lr_scheduler_type linear \
-    --learning_rate 7e-5 \
+    --learning_rate 3.5e-5 \
     --weight_decay 0.01 \
     --adam_beta1 0.9 \
     --adam_beta2 0.98 \
     --adam_epsilon 1e-6 \
     --bpe_dropout 0.2 \
     --activation_dropout 0.1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 8 \
     --preprocessing_num_workers 32 \
     --timestamp_column_name "timestamped_text" \
     --prev_column_name "previous_text" \
     --log_max_eval_predictions 100 \
     --log_eval_predictions_fn "utils.log_predictions.write_predictions" \
-    --log_examples 100 \
+    --log_examples 1 \
     --streaming True \
     --use_auth_token True \
     --dtype bfloat16 \
-    --dtype_params bfloat16 \
     --hub_private_repo True \
     --resume_from_checkpoint True \
     --ignore_data_skip \
