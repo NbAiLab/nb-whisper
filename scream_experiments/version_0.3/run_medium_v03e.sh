8fec49854570ffdb326f6b978608ed3445f50eac
Per Egil Kummervold <per@capia.no>
2023-12-03 10:22:52 +0000

    0.3

--- scream_experiments/version_0.3/run_medium_v03e.sh
+++ scream_experiments/version_0.3/run_medium_v03e.sh
@@ -7,8 +7,8 @@ export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 # Running the Python script
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --model_name_or_path NbAiLab/nb-whisper-medium-RC1 \
-    --run_name "NB-Whisper - medium - dynamic eval v0.3c" \
-    --run_description "A NB-Whisper dynamic eval v0.3c" \
+    --run_name "NB-Whisper - medium - dynamic eval v0.3e" \
+    --run_description "A NB-Whisper dynamic eval v0.3e" \
     --wandb_entity "nbailab" \
     --wandb_project "NB-Whisper dynamic v0.1" \
     --dataset_name NbAiLab/ncc_speech_styling_v3 \
@@ -18,8 +18,8 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --dataset_load_fn "utils.dataset_load_rc_dynamic_noellipses_turndupwords.load_dataset_nbwhisper_rc_dynamic_eval_verbatim" \
     --test_split_name "test_nst,test_clean_stortinget_no" \
     --eval_split_name "validation_nst,validation_clean_stortinget_no" \
-    --hub_model_id NbAiLab/nb-whisper-medium-v0.3c \
-    --output_dir ../../../nb-whisper-medium-v0.3c \
+    --hub_model_id NbAiLab/nb-whisper-medium-v0.3e \
+    --output_dir ../../../nb-whisper-medium-v0.3e \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
