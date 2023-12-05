3217c8b313f03808a35da2ac148a1219f19afca1
Per Egil Kummervold <per@capia.no>
2023-12-05 20:58:37 +0000

    npsc

--- scream_experiments/version_0.7_semantic/run_medium_v07_semantic.sh
+++ scream_experiments/version_0.7_semantic/run_medium_v07_semantic.sh
@@ -6,32 +6,30 @@ export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 
 # Running the Python script
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
-    --model_name_or_path NbAiLabBeta/nb-whisper-large \
-    --run_name "Scream - large-v07-verbatim-npsck" \
-    --run_description "A large NB-Whisper NPSC" \
+    --model_name_or_path NbAiLab/nb-whisper-medium-v0.3b \
+    --run_name "NB-Whisper - medium - dynamic eval v0.4d" \
+    --run_description "A NB-Whisper dynamic eval v0.4d" \
     --wandb_entity "nbailab" \
-    --wandb_project "NB-Whisper NPSC" \
-    --dataset_name NbAiLab/NPSC \
-    --dataset_config_name "16K_mp3" \
+    --wandb_project "NB-Whisper dynamic v0.1" \
+    --dataset_name NbAiLab/ncc_speech_styling_v4 \
     --language Norwegian \
-    --text_column_name normsentence_text \
+    --text_column_name text \
     --train_split_name train \
-    --dataset_load_fn "utils.dataset_load_npsc_both.load_dataset_npsc_norm_nohes" \
-    --test_split_name "test" \
-    --eval_split_name "validation" \
-    --max_eval_samples 1024 \
-    --hub_model_id NbAiLab/nb-whisper-large-v0.7-verbatim \
-    --output_dir ../../../nb-whisper-large-v0.7-verbatim \
+    --dataset_load_fn "utils.dataset_load_rc_dynamic_downsampled_notranscribe.load_dataset_nbwhisper_rc_dynamic_eval_verbatim" \
+    --test_split_name "test_nst,test_clean_stortinget_no" \
+    --eval_split_name "validation_nst,validation_clean_stortinget_no" \
+    --hub_model_id NbAiLab/nb-whisper-medium-v0.4d \
+    --output_dir ../../../nb-whisper-medium-v0.4d \
     --overwrite_output_dir \
     --do_train \
     --do_predict \
     --do_eval \
     --predict_with_generate \
-    --warmup_steps 40 \
-    --num_train_steps 250 \
-    --eval_steps 40 \
+    --warmup_steps 10 \
+    --num_train_steps 100 \
+    --eval_steps 5 \
     --lr_scheduler_type linear \
-    --learning_rate 7e-5 \
+    --learning_rate 2.5e-5 \
     --weight_decay 0.01 \
     --adam_beta1 0.9 \
     --adam_beta2 0.98 \
@@ -39,8 +37,10 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --bpe_dropout 0.2 \
     --activation_dropout 0.1 \
     --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 1 \
+    --per_device_eval_batch_size 8 \
     --preprocessing_num_workers 32 \
+    --timestamp_column_name "timestamped_text" \
+    --prev_column_name "previous_text" \
     --log_max_eval_predictions 100 \
     --log_eval_predictions_fn "utils.log_predictions.write_predictions" \
     --log_examples 100 \
@@ -51,7 +51,7 @@ python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
     --resume_from_checkpoint True \
     --ignore_data_skip \
     --gradient_checkpointing True \
-    --push_to_hub_auto_lfs_prune True \
     --pad_target_to_multiple_of 400 \
     --max_prev_length 120 \
+    --push_to_hub_auto_lfs_prune True \
     --push_to_hub
