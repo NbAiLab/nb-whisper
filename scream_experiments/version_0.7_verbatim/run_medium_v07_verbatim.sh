17a5cf3d49c0e2b00270d17e3a77cc488ca0825b
Per Egil Kummervold <per@capia.no>
2023-12-05 19:35:49 +0000

    npsc

--- scream_experiments/version_0.7_verbatim/run_medium_v07_verbatim.sh
+++ scream_experiments/version_0.7_verbatim/run_medium_v07_verbatim.sh
@@ -6,7 +6,7 @@ export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=10000000000
 
 # Running the Python script
 python ../../run_nb_flax_speech_recognition_seq2seq_streaming_dev.py \
-    --model_name_or_path NbAiLab/nb-whisper-medium-v0.7-verbatim \
+    --model_name_or_path NbAiLabBeta/nb-whisper-medium \
     --run_name "Scream - medium-v07-verbatim-npsck" \
     --run_description "A Medium NB-Whisper NPSC" \
     --wandb_entity "nbailab" \
